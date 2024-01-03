f74e9595c4354632ae51f71d3a9e82b791079673
Hienphan0111 <hienphan@digitalcad.edu.vn>
2024-01-03 17:51:27 +0700

    Add more routes

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,7 +1,15 @@
 package controllers
 
 import (
+	"log"
+	"net/http"
+
+	"context"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/hienphan0111/ecommerce-api/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // HashPassword is a function that hashes the password
@@ -16,7 +24,35 @@ func VerifyPassword(password string, hashedPassword string) (bool, string) {
 
 // SignUp is a function that signs up a user
 func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email already exists"})
+			return
+		}
+
+	}
 }
 
 // Login is a function that logs in a user
--- database/databasesetup.go
+++ database/databasesetup.go
@@ -41,9 +41,11 @@ func DBSet() *mongo.Client {
 var Client *mongo.Client = DBSet()
 
 func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var collection *mongo.Collection = client.Database("ecommerce").Collection(collectionName)
+	return collection
 }
 
 func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+	var productCollection *mongo.Collection = client.Database("ecommerce").Collection(collectionName)
+	return productCollection
 }
